d9076ffac8f4b376932b155bde6b345978a15bf6
davido2003 <70234277+davido2003@users.noreply.github.com>
2021-09-25 20:30:23 +0100

    Update urls.py

--- folder/urls.py
+++ folder/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ProfileAPIView
 
 urlpatterns = [
-    path('/', ProfileAPIView.as_view()),
-    path('/', ProfileAPIView.as_view())
+    path('profile/', ProfileAPIView.as_view()),
+    path('profile/', ProfileAPIView.as_view())
     #path('profile/', profile_list),
    # path('detail/<int:pk>/', profile_detail)
 ]
